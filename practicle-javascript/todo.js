87bde369ba8076e1e9708afcf1e8696021c4fa87
Craig Clayton <mancityvsliverpoolfc@outlook.com>
2020-01-29 13:40:01 +0000

    .

--- practicle-javascript/todo.js
+++ practicle-javascript/todo.js
@@ -1,26 +1,27 @@
 var todoList = {
   todos: [],
-  displayTodos: function () {
-    console.log('My Todos', this.todos)
+  displayTodos: function() {
+    console.log("My Todos", this.todos);
   },
-  addTodo: function (todoText) {
+  addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
     this.displayTodos();
   },
-  changeTodo: function (position, todoText) {
+  changeTodo: function(position, todoText) {
     this.todos[position].todoText = todoText;
     this.displayTodos();
   },
-  deleteTodo: function (position) {
+  deleteTodo: function(position) {
     this.todos.splice(position, 1);
     this.displayTodos();
   },
-  toggleCompleted: function (position) {
+  toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
   }
 };
+console.log(todoList);
