7a55fd2fc685fbd865e6cc15a55c920764704f6d
Craig Clayton <mancityvsliverpoolfc@outlook.com>
2020-01-29 14:28:05 +0000

    added if todos === 0 then console.log

--- practicle-javascript/todo.js
+++ practicle-javascript/todo.js
@@ -1,7 +1,14 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log("My Todos", this.todos);
+    if (this.todos.length === 0) {
+      console.log("Your todo list is empty!");
+    } else {
+      console.log("My Todos:");
+      for (var i = 0; i < this.todos.length; i++) {
+        console.log(this.todos[i].todoText);
+      }
+    }
   },
   addTodo: function(todoText) {
     this.todos.push({
@@ -24,4 +31,5 @@ var todoList = {
     this.displayTodos();
   }
 };
-console.log(todoList);
+
+todoList.displayTodos();
